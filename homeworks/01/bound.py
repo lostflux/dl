17ba08b274752d55d84774b6d87ab8ffcf287bda
siavava <amittaijoel@outlook.com>
2023-04-23 14:55:03 -0400

    init

---
+++ homeworks/01/bound.py
@@ -0,0 +1,123 @@
+import torch
+import math
+
+# This module calculates different measures on your trained model and returns a dictionary whose keys are the measure names
+def calculate(model, init_model, device, train_loader, margin):
+
+    # switch to evaluate mode
+    model.eval()
+    init_model.eval()
+
+    modules = list(model.children())
+    init_modules = list(init_model.children())
+
+    D = modules[0].weight.size(1) # data dimension
+    H = modules[0].weight.size(0) # number of hidden units
+    C = modules[2].weight.size(0) # number of classes (output dimension)
+    num_param = sum(p.numel() for p in model.parameters()) # number of parameters of the model
+
+    with torch.no_grad():
+        # Eigenvalues of the weight matrix in the first layer
+        _,S1,_ = modules[0].weight.svd()
+        # Eigenvalues of the weight matrix in the second layer
+        _,S2,_ = modules[2].weight.svd()
+        # Eigenvalues of the initial weight matrix in the first layer
+        _,S01,_ = init_modules[0].weight.svd()
+        # Eigenvalues of the initial weight matrix in the second layer
+        _,S02,_ = init_modules[2].weight.svd()
+        #Frobenius norm of the weight matrix in the first layer
+        #************You will compute Fr1 below*******************
+        Fr1 = 
+
+        #*************Your code ends here*************************
+        # Frobenius norm of the weight matrix in the second layer
+        Fr2 = modules[2].weight.norm()
+        # difference of final weights to the initial weights in the first layer
+        #*************You will compute diff1 below***************** 
+        diff1= 
+
+        #***************Your code ends here************************
+        #Euclidean distance of the weight matrix in the first layer to the initial weight matrix
+        #**************You will compute Dist1 below using your diff1***************
+        Dist1 = 
+
+        #***************Your code ends here*************************
+        # difference of final weights to the initial weights in the second layer
+        diff2 = modules[2].weight - init_modules[2].weight
+        # Euclidean distance of the weight matrix in the second layer to the initial weight matrix
+        Dist2 = diff2.norm()
+        # L_{1,infty} norm of the weight matrix in the first layer
+        L1max1 = modules[0].weight.norm(p=1, dim=1).max()
+        # L_{1,infty} norm of the weight matrix in the second layer
+        L1max2 = modules[2].weight.norm(p=1, dim=1).max()
+        # L_{2,1} distance of the weight matrix in the first layer to the initial weight matrix
+        L1Dist1 = diff1.norm(p=2, dim=1 ).sum()
+        # L_{2,1} distance of the weight matrix in the second layer to the initial weight matrix
+        L1Dist2 = diff2.norm(p=2, dim=1 ).sum()
+
+        measure = {}
+        measure['Frobenius1'] = Fr1
+        measure['Frobenius2'] = Fr2
+        measure['Distance1'] = Dist1
+        measure['Distance2'] = Dist2
+        measure['Spectral1'] = S1[0]
+        measure['Spectral2'] = S2[0]
+        measure['Fro_Fro'] = Fr1 * Fr2
+        measure['L1max_L1max'] = L1max1 * L1max2
+        measure['Spec_Dist'] = S1[0] * Dist2 * math.sqrt(C)
+        measure['Dist_Spec'] = S2[0] * Dist1 * math.sqrt(H)
+        measure['Spec_Dist_sum'] = measure['Spec_Dist'] + measure['Dist_Spec']
+        measure['Spec_L1max'] = S1[0] * L1Dist2
+        measure['L1max_Spec'] = S2[0] * L1Dist1
+        measure['Spec_L1max_sum'] = measure['Spec_L1max'] + measure['L1max_Spec']
+        #****************You will compute the Frobenius Distance using the provided instruction************
+        measure['Dist_Fro'] = 
+        
+        #******************Your code ends here***********************
+        # delta is the probability that the generalization bound does not hold
+        delta = 0.01
+        # m is the number of training samples
+        m = len(train_loader.dataset)
+        layer_norm, data_L2, data_Linf, domain_L2 = 0, 0, 0, 0
+        for i, (data, target) in enumerate(train_loader):
+            data = data.to(device).view(target.size(0),-1)
+            layer_out = torch.zeros(target.size(0), H).to(device)
+
+            # calculate the norm of the output of the first layer in the initial model
+            def fun(m, i, o): layer_out.copy_(o.data)
+            h = init_modules[1].register_forward_hook(fun)
+            output = init_model(data)
+            layer_norm += layer_out.norm(p=2, dim=0) ** 2
+            h.remove()
+
+            # L2 norm squared of the data 
+            data_L2 += data.norm() ** 2
+            # maximum L2 norm squared on the training set. We use this as an approximation of this quantity over the domain
+            domain_L2 = max(domain_L2, data.norm(p=2, dim = 1).max() ** 2)
+            # L_infty norm squared of the data
+            data_Linf += data.max(dim = 1)[0].max() ** 2
+
+        # computing the average
+        data_L2 /= m
+        data_Linf /= m
+        layer_norm /= m
+
+        # number of parameters
+        measure['#parameter'] = num_param
+
+        # Generalization bound based on the VC dimension by Harvey et al. 2017
+        VC = (2 + num_param * math.log(8 * math.e * ( H + 2 * C ) * math.log( 4 * math.e * ( H + 2 * C ) ,2), 2)
+                * (2 * (D + 1) * H + (H + 1) * C) / ((D + 1) * H + (H + 1) * C))
+        measure['VC bound'] = 8 * (C * VC * math.log(math.e * max(m / VC, 1))) + 8 * math.log(2 / delta)
+
+        # Generalization bound by Bartlett and Mendelson 2002
+        R = 8 * C * L1max1 * L1max2 * 2 * math.sqrt(math.log(D)) * math.sqrt(data_Linf) / margin
+        measure['L1max bound'] = (R + 3 * math.sqrt(math.log(m / delta))) ** 2
+
+        # Here is the below part you will complete 
+        #********************Compute the Generalization bound as provided in the instruction***************
+        measure['Your computed bound'] = 
+        
+        #*********************Your code ends here*********************
+        
+    return measure
---
+++ homeworks/01/main.py
@@ -0,0 +1,173 @@
+import numpy as np
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+import copy
+import bound
+import argparse
+from torchvision import transforms, datasets
+
+# train the model for one epoch on the given dataset
+def train(model, device, train_loader, criterion, optimizer, epoch):
+    sum_loss, sum_correct = 0, 0
+
+    # switch to train mode
+    model.train()
+
+    for i, (data, target) in enumerate(train_loader):
+        data, target = data.to(device).view(data.size(0),-1), target.to(device)
+
+        # compute the output
+        output = model(data)
+
+        # compute the classification error and loss
+        loss = criterion(output, target)
+        pred = output.max(1)[1]
+        sum_correct += pred.eq(target).sum().item()
+        sum_loss += len(data) * loss.item()
+
+        # compute the gradient and do an SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    return 1 - (sum_correct / len(train_loader.dataset)), sum_loss / len(train_loader.dataset)
+
+
+# evaluate the model on the given set
+def validate(model, device, val_loader, criterion):
+    sum_loss, sum_correct = 0, 0
+    margin = torch.Tensor([]).to(device)
+
+    # switch to evaluation mode
+    model.eval()
+    with torch.no_grad():
+        for i, (data, target) in enumerate(val_loader):
+            data, target = data.to(device).view(data.size(0), -1), target.to(device)
+
+            # compute the output
+            output = model(data)
+
+            # compute the classification error and loss
+            pred = output.max(1)[1]
+            sum_correct += pred.eq(target).sum().item()
+            sum_loss += len(data) * criterion(output, target).item()
+
+            # compute the margin
+            output_m = output.clone()
+            for i in range(target.size(0)):
+                output_m[i, target[i]] = output_m[i,:].min()
+
+            #****************You will compute the margin below of your network*****************    
+            margin = 
+
+            #*************Your code ends here*************************
+
+        #****************Return 5th percentile below********************
+        percentile_margin = 
+
+        #*************Your code ends here*************************
+    return 1 - (sum_correct / len(val_loader.dataset)), sum_loss / len(val_loader.dataset), percentile_margin
+
+
+# Load and Preprocess CIFAR-10 data.
+# Loading: Dataset will be downloaded if the dataset is not in the given directory
+# Preprocessing: You have to do normalizing each channel and data augmentation by random cropping and horizontal flipping
+def load_data(split, dataset_name, datadir, nchannels):
+
+    ##Normalize dataset
+    ##mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    ##*************Write the code below to Normalize and do data augmentation*******************
+    normalize = 
+    tr_transform = 
+    val_transform = 
+
+    #*************Your code ends here*************************
+
+    ##Load the datasets with the pre-processing
+    get_dataset = getattr(datasets, dataset_name)
+    
+    if split == 'train':
+        dataset = get_dataset(root=datadir, train=True, download=True, transform=tr_transform)
+    else:
+        dataset = get_dataset(root=datadir, train=False, download=True, transform=val_transform)
+
+    return dataset
+
+
+# This function trains a FC neural net with a singler hidden layer on the given dataset and calculates
+# various measures on the learned network
+# You have to write the parameters according to instructions, define the network and train it
+def main():
+
+    #Settings
+    #Define the parameters to train your model
+    #***********Check the instructions and make sure you set your parameters accordingly*********
+    datadir='datasets'
+    dataset="CIFAR10"
+    nunits=
+    lr=
+    mt=
+    batchsize=
+    epochs=
+    stopcond=
+
+    #*************Your code ends here*************************
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    kwargs = {'num_workers': 1, 'pin_memory': True} if device else {}
+    nchannels, nclasses = 3, 10
+    if (dataset == 'MNIST'): 
+      nchannels = 1
+    if (dataset == 'CIFAR100'): 
+      nclasses = 100
+
+    # create an initial model
+    #*****************Define your 2-layer model here*****************
+    model = 
+
+    #*************Your code ends here********************************
+
+    model = model.to(device)
+
+    # create a copy of the initial model to be used later
+    init_model = copy.deepcopy(model)
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(model.parameters(), lr, momentum=mt)
+
+    # loading data
+    train_dataset = load_data('train', dataset, datadir, nchannels)
+    val_dataset = load_data('val', dataset, datadir, nchannels)
+
+    train_loader = DataLoader(train_dataset, batch_size=batchsize, shuffle=True, **kwargs)
+    val_loader = DataLoader(val_dataset, batch_size=batchsize, shuffle=False, **kwargs)
+
+    # training the model
+    for epoch in range(0, epochs):
+        # *************You have to complete the code below for training your model****************
+
+        tr_err, tr_loss = 
+        #Validation run
+        val_err, val_loss, val_margin = 
+
+       #*************Your code ends here********************************
+
+        print(f'Epoch: {epoch + 1}/{epochs}\t Training loss: {tr_loss:.3f}\t Validation margin {val_margin:.3f}\t ',
+                f'Training error: {tr_err:.3f}\t Validation error: {val_err:.3f}')
+
+        # stop training if the cross-entropy loss is less than the stopping condition
+        if tr_loss < stopcond: 
+            break
+    # calculate the training error and margin (on Training set) of the learned model
+    tr_err, tr_loss, tr_margin = validate( model, device, train_loader, criterion)
+    print(f'\nFinal: Training loss: {tr_loss:.3f}\t Training margin {tr_margin:.3f}\t ',
+            f'Training error: {tr_err:.3f}\t Validation error: {val_err:.3f}\n')
+    # Print the measures and bounds computed in bound.py
+    measure = bound.calculate(model, init_model, device, train_loader, tr_margin)
+    for key, value in measure.items():
+        print(f'{key:s}:\t {float(value):3.3}')
+    
+
+if __name__ == '__main__':
+    main()
